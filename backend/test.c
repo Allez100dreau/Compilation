d555e196c7136040c0601c80c5756fc763aa0524
Allez100dreau <Allez100dreau@github.com>
2020-05-17 11:48:52 +0200

    Génération de code pour les déclarations.

--- backend/test.c
+++ backend/test.c
@@ -1,20 +1,3 @@
-
-goto Ltest1;
-Lwhile1:
-$3
-Ltest1:
-if ($1) goto Lwhile1;
-
-$1
-goto Ltest1;
-Lfor1:
-$5
-$3
-Ltest1:
-if ($2) goto Lfor1;
-
-goto Ltest1;
-Lwhile1:
-$3
-Ltest1:
-if ($1) goto Lwhile1;
+int printd;
+int i;
+int j;
